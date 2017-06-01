3a946150c81bf6299967898cfb8a7111a7eb1b71
zzhang115 <zzhang115@dons.usfca.edu>
2017-05-31 21:13:02 -0700

    run manage.py on pycharm

--- Django/urls.py
+++ Django/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from views import sayHi, saytime, cpu
+from views import sayHi, saytime, cpu, hours_ahead
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^sayHi/$', sayHi),
     url(r'^saytime/$', saytime),
-    url(r'^cpu/$', cpu)
+    url(r'^cpu/$', cpu),
+    url(r'^hours/plus/\d+/$', hours_ahead)
 ]
--- Django/views.py
+++ Django/views.py
@@ -1,4 +1,4 @@
-from django.http import HttpResponse
+from django.http import Http404, HttpResponse
 import datetime, time, os
 # import django
 # django.http.HttpResponse()
@@ -12,7 +12,18 @@ def saytime(request):
     html = "<html><body>It is %s<br/>format time: %s</body></html>" %(now, newtime)
     return HttpResponse(html)
     #//it can output html
+
 def cpu(request):
     cpu_status = os.popen('sar 1 1').read()
     html = "<html><body><pre> %s</pre></body>" %cpu_status
     return HttpResponse(html)
+
+def hours_ahead(request, offset):
+   try:
+       offset = int(offset)
+   except ValueError:
+       raise Http404()
+   dt = datetime.datetime.now() + datetime.timedelta(hours=offset)
+   html = "<html><body>In %s hours, it will be %s.</body></html>" %(offset, dt)
+   return HttpResponse(html)
+
