7b392485f160a7ad5daecf2744e989997e30aaee
zzhang115 <zzhang115@dons.usfca.edu>
2017-06-01 21:48:02 -0700

    add mysql setting.py

--- Django/settings.py
+++ Django/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'Django.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': '/var/lib/mysql/testdb',#os.path.join(BASE_DIR, 'db.mysql'),
+        'USER': 'root',
+        'PASSWORD': '238604',
+
     }
 }
 
